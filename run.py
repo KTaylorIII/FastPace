cc86e2dbf96101c0288ea665b2827293dafd05b5
Kent 'Austin' Taylor <Austin.Tayloriii@yahoo.com>
2016-08-15 14:55:06 +0000

    Fixed initializer bugs involving invalid SQL syntax. Separated main code into appropriate main function. Added new command line parser for improved UX.

--- run.py
+++ run.py
@@ -1,13 +1,24 @@
 from spider.scanner import *
 from views.argparser import Argparser
-if __name__=='__main__':
+
+
+
+def main():
     parser = Argparser();
     args = parser.parse();
-
-    scanner = Scanner(clear_db=args['clear'], verbosity=args['verbosity']);
     
     if args['prompt']:
         url = raw_input('[?] Enter a .onion domain: ');
-        scanner.start(url);
+        Scanner(clear_db=args['clear'], verbosity=args['verbosity']).start(url);
 
-    
+    else:
+        if not args['domain']:
+
+            if args['verbosity']:
+
+                print '[-] ERROR: You must specify the "prompt" flag or supply a valid .onion ID';
+        else:
+            scanner = Scanner(clear_db=args['clear'], verbosity=args['verbosity'])
+            scanner.start(args['domain']);
+if __name__=='__main__':
+    main();
